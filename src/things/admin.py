0e8711f209b375f4fb75f299e1fed3deaa50f49e
Milosz <sobiczewski.milosz@gmail.com>
2020-04-04 22:40:42 +0200

    add bought indicator

--- src/things/admin.py
+++ src/things/admin.py
@@ -14,6 +14,7 @@ class PlanAdmin(admin.ModelAdmin):
         "date",
         "comment",
         "checks",
+        "bought",
     ]
 
     def checks(self, obj):
--- src/things/models.py
+++ src/things/models.py
@@ -6,6 +6,7 @@ class Thing(models.Model):
     date = models.DateField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     approved = models.BooleanField(default=False)
+    bought = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.thing}"
